2d9d0cd6116336cbb06e4cf37c11ab17c22a4e20
priyanshup847 <priyanshup847@gmail.com>
2025-08-17 21:54:17 +0530

    minor chng

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
-i am dhondhu
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = "5c8c34db-1cec-41ed-9c43-cb5372d74ff4"
+        features {
+        
+        }
+}
+resource "azurerm_resource_group" "priyanshu-rg" {
+  name     = "rg-priyanshu"
+  location = "West Europe"
+}
